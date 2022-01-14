d87acded7d771131cae1b5815f2d927445626931
byeongjeon jeon <pensieve92@gmail.com>
2022-01-14 22:40:18 +0900

    env 파일과 설정 방법

--- src/api/index.js
+++ src/api/index.js
@@ -1,18 +1,11 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: process.env.VUE_APP_API_RUL,
 });
 
 function registerUser(userData) {
-  // const url = 'http://localhost:3000/signup';
-  // return axios.post(url, userData);
-  return instance.post('/signup', userData);
+  return instance.post('signup', userData);
 }
-
-// function loginUser() {
-//   const url = '';
-//   return axios.post(url);
-// }
 
 export { registerUser };
