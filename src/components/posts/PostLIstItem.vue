98c51d3dba968d6ff9583f1402e846955e2e60e6
byeongjeon jeon <pensieve92@gmail.com>
2022-01-15 22:30:21 +0900

    학습 노트 데이터 조회

---
+++ src/components/posts/PostLIstItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul>
+    <li>
+      <div class="post-title">{{ postItem.title }}</div>
+      <div class="post-contents">{{ postItem.contents }}</div>
+      <div class="post-time">{{ postItem.createdAt }}</div>
+    </li>
+  </ul>
+</template>
+<script>
+export default {
+  name: 'PostItems',
+  props: {
+    postItem: {
+      type: Object,
+      require: true,
+    },
+  },
+};
+</script>
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,13 +1,42 @@
 <template>
   <div class="contents main">
     <h1>Today I Learned</h1>
+    <LoadingSpinner v-if="isLoading"></LoadingSpinner>
+    <ul v-else>
+      <PostLIstItem
+        v-for="postItem in postItems"
+        :key="postItem._id"
+        :postItem="postItem"
+      />
+    </ul>
   </div>
 </template>
 
 <script>
+import { fetchPosts } from '@/api';
+import LoadingSpinner from '@/components/common/LoadingSpinner';
+import PostLIstItem from '@/components/posts/PostLIstItem';
+
 export default {
   name: 'MainPage',
+  components: { PostLIstItem, LoadingSpinner },
+  data() {
+    return {
+      postItems: [],
+      isLoading: false,
+    };
+  },
+  methods: {
+    async fetchData() {
+      this.isLoading = true;
+      const { data } = await fetchPosts();
+      console.log(data);
+      this.isLoading = false;
+      this.postItems = data.posts;
+    },
+  },
+  created() {
+    this.fetchData();
+  },
 };
 </script>
-
-<style scoped></style>
