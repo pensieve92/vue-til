c02460557902d1c1f76e69735ce90785297a447a
byeongjeon jeon <pensieve92@gmail.com>
2022-01-15 17:19:31 +0900

    엑시오스 인터셉터 모듈화 및 인스턴스에 연결하는 방법

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,17 @@
 import axios from 'axios';
 import store from '@/store';
+import { setInterceptors } from '@/api/common/interceptors';
 
-const instance = axios.create({
-  baseURL: process.env.VUE_APP_API_RUL,
-  headers: {
-    Authorization: store.state.token,
-  },
-});
+function createInstance() {
+  const instance = axios.create({
+    baseURL: process.env.VUE_APP_API_RUL,
+    headers: {
+      Authorization: store.state.token,
+    },
+  });
+  return setInterceptors(instance);
+}
+const instance = createInstance();
 
 function registerUser(userData) {
   return instance.post('signup', userData);
