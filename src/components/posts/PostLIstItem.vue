bbb5d4f280019d6a9e9ec91d4bcbd5ad7aa8e0fe
byeongjeon jeon <pensieve92@gmail.com>
2022-01-16 18:45:32 +0900

    삭제 API 함수 및 기능 구현

--- src/components/posts/PostLIstItem.vue
+++ src/components/posts/PostLIstItem.vue
@@ -4,16 +4,25 @@
       <div class="post-title">{{ postItem.title }}</div>
       <div class="post-contents">{{ postItem.contents }}</div>
       <div class="post-time">{{ postItem.createdAt }}</div>
+      <i class="icon ion-md-create"></i>
+      <i class="icon ion-md-trash" @click="deleteItem"></i>
     </li>
   </ul>
 </template>
 <script>
+import { deletePost } from '@/api/posts';
+
 export default {
   name: 'PostItems',
   props: {
     postItem: {
       type: Object,
       require: true,
+    },
+  },
+  methods: {
+    async deleteItem() {
+      await deletePost(this.postItem._id);
     },
   },
 };
