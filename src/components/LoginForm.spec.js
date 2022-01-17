bbff53a2d9af6bcf83bb3bf8171eb46931bfb300
byeongjeon jeon <pensieve92@gmail.com>
2022-01-17 22:20:33 +0900

    뷰 테스트 유틸 라이브러리 소개 및 적용

--- src/components/LoginForm.spec.js
+++ src/components/LoginForm.spec.js
@@ -1,10 +1,14 @@
+import { shallowMount } from '@vue/test-utils';
 import Vue from 'vue';
 import LoginForm from '@/components/LoginForm';
 
 describe('LoginForm.vue', () => {
   test('컴포넌트가 마운팅되면 username이 존재하고 초기 값으로 설정되어 있어야한다. ', () => {
-    const instance = new Vue(LoginForm).$mount();
-    console.log(instance.username);
-    expect(instance.username).toBe('');
+    // const instance = new Vue(LoginForm).$mount();
+    //   console.log(instance.username);
+    //   expect(instance.username).toBe('');
+
+    const wrapper = shallowMount(LoginForm);
+    expect(wrapper.vm.username).toBe('');
   });
 });
