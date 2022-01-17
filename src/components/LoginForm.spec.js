d299ecfdcd5accb12bda780750f729e942745b3e
byeongjeon jeon <pensieve92@gmail.com>
2022-01-17 22:09:22 +0900

    테스트 코드 작성 팁

--- src/components/LoginForm.spec.js
+++ src/components/LoginForm.spec.js
@@ -5,6 +5,7 @@ sum(10, 20); //30
 describe('math.js', () => {
   test('10 + 20 = 30', () => {
     const result = sum(10, 20);
-    expect(result).toBe(30);
+    // expect(result).toBe(12);
+    expect(result).not.toBe(30);
   }); // 하나의 테스트 케이스를 검증하는 API
 });
