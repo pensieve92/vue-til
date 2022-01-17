e69066fae4e7f36cc06e522e8e34a73ab8edaa88
byeongjeon jeon <pensieve92@gmail.com>
2022-01-17 22:05:30 +0900

    자바스크립트 테스트 코드 시작하기

---
+++ src/components/LoginForm.spec.js
@@ -0,0 +1,10 @@
+import { sum } from '@/components/math';
+
+sum(10, 20); //30
+
+describe('math.js', () => {
+  test('10 + 20 = 30', () => {
+    const result = sum(10, 20);
+    expect(result).toBe(30);
+  }); // 하나의 테스트 케이스를 검증하는 API
+});
