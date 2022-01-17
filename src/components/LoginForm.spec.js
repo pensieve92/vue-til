8b956e045b37325df006a8a129755c96d6628225
byeongjeon jeon <pensieve92@gmail.com>
2022-01-17 22:14:46 +0900

    뷰 컴포넌트 테스트 방법

--- src/components/LoginForm.spec.js
+++ src/components/LoginForm.spec.js
@@ -1,11 +1,10 @@
-import { sum } from '@/components/math';
+import Vue from 'vue';
+import LoginForm from '@/components/LoginForm';
 
-sum(10, 20); //30
-
-describe('math.js', () => {
-  test('10 + 20 = 30', () => {
-    const result = sum(10, 20);
-    // expect(result).toBe(12);
-    expect(result).not.toBe(30);
-  }); // 하나의 테스트 케이스를 검증하는 API
+describe('LoginForm.vue', () => {
+  test('컴포넌트가 마운팅되면 username이 존재하고 초기 값으로 설정되어 있어야한다. ', () => {
+    const instance = new Vue(LoginForm).$mount();
+    console.log(instance.username);
+    expect(instance.username).toBe('');
+  });
 });
